1a50646c724f4254f8d3ae9275d71421a7a89f56
Igor' Khan <igor.swit4@gmail.com>
2019-08-12 13:45:34 +1000

    Блок "edu slider" готов

--- build/assets/js/blocks.js
+++ build/assets/js/blocks.js
@@ -80,7 +80,31 @@ jQuery(($) => {
     carousel = $jqCarousel.owlCarousel(owlConfig);
 
 });
+jQuery(($) => {
+    let owlConfig,
+        $jqCarousel,
+        carousel,
+        slide_delay = 1,
+        reset_timeout = 1300;
 
+    owlConfig = {
+        items: 2,
+        margin: 20,
+        loop: true,
+        URLhashListener: true,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true
+//        navigation: true
+    };
+    $jqCarousel = $('.js-edu-slider');
+
+    carousel = $jqCarousel.owlCarousel(owlConfig);
+
+});
+
+//
 jQuery(($) => {
     let owlConfig,
         $jqCarousel,
